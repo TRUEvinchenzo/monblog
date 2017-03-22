aa5bf4bba72f40af86bb2c25592c347349da4b12
Vincent Le Bars <vincent.lebars@siochaptalqper.fr>
2017-03-22 14:53:12 +0100

    - création du fichier modele.php pour contenir l'accès aux données par la fonction getBillet(); - modification du fichier index.php pour contenir l'appel de la fonction getBillet et les require des fichiers modele.php et vueAccueil.php

---
+++ modele.php
@@ -0,0 +1,9 @@
+<?php
+/*Accèes aux données*/
+function getBillet(){
+	$bdd = new PDO('mysql:host=localhost;dbname=monblog;charset=utf8','ts2', 'ts2');
+	$billets = $bdd->query('select BIL_ID as id, BIL_DATE as date,'	. ' BIL_TITRE as titre, BIL_CONTENU as contenu from T_BILLET'. ' order by BIL_ID desc');
+	return $billets;
+}
+
+?>
